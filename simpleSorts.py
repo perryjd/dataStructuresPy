522a509f5fa7e9fec7130758ada0199c78371d30
Justin Perry <perryj89@gmail.com>
2015-03-06 15:00:50 -0500

    added implementations of selection sort and insertion sort

--- simpleSorts.py
+++ simpleSorts.py
@@ -1,3 +1,5 @@
+import random
+
 class bubArray:
 	def __init__(self):
 		self.w = []
@@ -16,13 +18,29 @@ class bubArray:
 			for inner in range (0, outer):
 				 if self.w[inner] > self.w[inner + 1]:
 				 	self.swap(inner, inner + 1)
+	def selectSort(self):
+		for outer in range (0, self.nElems() - 1):
+			smallest = outer
+			for inner in range (outer + 1, self.nElems()):
+				if self.w[inner] < self.w[smallest]:
+					smallest = inner
+			self.swap(outer, smallest)
+	def insertSort(self):
+		for outer in range (1, self.nElems()):
+			temp = self.w[outer]
+			inner = outer
+			while inner > 0 and self.w[inner - 1] >= temp:
+				self.w[inner] = self.w[inner - 1]
+				inner -= 1
+			self.w[inner] = temp
+
 
 myArray = bubArray()
 for x in range (10, -1, -1):
-	myArray.insert(x)
+	myArray.insert(random.randrange(1, 100))
 
 myArray.display()
 
-myArray.bubbleSort()
+myArray.insertSort()
 
 myArray.display()
